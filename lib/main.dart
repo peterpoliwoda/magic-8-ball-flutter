5884d1328f938c8079217d529adfe2040dd05691
Peter Poliwoda <peterpoliwoda@ie.ibm.com>
2020-10-04 15:09:52 +0100

    Finished tutorial challenge. Magic 8-ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,43 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blueAccent,
+          ),
+          body: Magic8BallPage(),
+        ),
       ),
     );
+
+class Magic8BallPage extends StatefulWidget {
+  @override
+  _Magic8BallPageState createState() => _Magic8BallPageState();
+}
+
+class _Magic8BallPageState extends State<Magic8BallPage> {
+  int randomAdvice = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Row(
+      children: <Widget>[
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              setState(() {
+                randomAdvice = Random().nextInt(5) + 1;
+              });
+            },
+            child: Image.asset('images/ball$randomAdvice.png'),
+          ),
+        )
+      ],
+    ));
+  }
+}
